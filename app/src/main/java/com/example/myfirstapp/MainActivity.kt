e2ffcd5db51c9cee0cf2fca177233965e7382de4
Reymon <sebastestudia@gmail.com>
2022-11-05 05:24:54 -0500

    happy face

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -10,8 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        //:)
         val recycler = findViewById<RecyclerView>(R.id.recycler)
 
         val layoutManager = GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false)
