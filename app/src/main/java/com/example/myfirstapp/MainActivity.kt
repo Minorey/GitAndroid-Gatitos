06575f75d4d5629b6196a2d204cd69bfa6586ceb
Reymon <sebastestudia@gmail.com>
2022-11-05 05:21:30 -0500

    delete prints

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -10,14 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("esto es un ejemplo")
-        print("esto es un ejemplo")
-        print("esto es un ejemplo")
-        print("esto es un ejemplo")
-        print("esto es un ejemplo")
-        print("esto es un ejemplo")
-        print("esto es un ejemplo")
-        print("esto es un ejemplo")
+
 
         val recycler = findViewById<RecyclerView>(R.id.recycler)
 
